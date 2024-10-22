4c87a1fec63823cad6896ed33e749de1b60106dc
Patrick Carvalho <patrickcarvalho448@gmail.com>
2024-10-22 23:02:18 +0200

    add division and multiplication

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -9,4 +9,12 @@ public class Calculator {
     public int subtraction(int a, int b){
         return a + b;
     }
+
+    public int multiplication(int a, int b){
+        return a*b;
+    }
+
+    public int division(int a, int b){
+        return a/b;
+    }
 }
