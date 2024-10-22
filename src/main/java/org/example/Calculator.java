94de222589b95a5d6d3f9e8f7d2e877bcdbbd754
Patrick Carvalho <patrickcarvalho448@gmail.com>
2024-10-22 22:37:06 +0200

    Add addition and subtraction functions

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -2,5 +2,11 @@ package org.example;
 
 public class Calculator {
 
+    public int addition(int a, int b){
+        return a + b;
+    }
 
+    public int subtraction(int a, int b){
+        return a + b;
+    }
 }
